9c35b75fb26290f2bf917a526f1a1b77ad35c624
Manav1207351 <Manav1207351@users.noreply.github.com>
2026-03-05 08:45:59 +0000

    deploy: 24d9b6fba4b86c68d317000011b9cff2e1534a2c

---
+++ _next/static/8WsHgM87oHTHp9CtMUL2H/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Manav-Mule//_next/:path+",
+        "destination": "/Manav-Mule/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
